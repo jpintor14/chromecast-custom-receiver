66ae5ecc7c1210e7e2abc251cb44d3e38e8ee649
Javier Pintor <javier.pintor@optivamedia.com>
2020-04-13 18:54:21 +0200

    Create WasABI js class

---
+++ custom/WasABI.js
@@ -0,0 +1,67 @@
+//
+//  WasABI.swift
+//  Orange TV
+//  WasABI ABI integrator (RTV, Compass, SB, DCS)
+//  Created by Optiva Media on 13/04/2020.
+//  Copyright © 2020 Orange. All rights reserved.
+//
+class WasABI {
+
+  constructor() {
+    this.channel = 'urn:x-cast:com.optm.anhplayer';
+    this.licenseUrl = 'https://ios.orangetv.orange.es/mob/api/rtv/v1/drm';
+    this.rightTvUrl = 'https://orangetv.orange.es/mob/api/rtv/v1';
+    this.username = '';
+    this.password = '';
+  }
+
+  // chromecast custom namespace
+  get getChannel() {
+    return this.channel;
+  }
+  
+  // drm licenser url
+  get getLicenseUrl() {
+    return this.licenseUrl;
+  }
+
+  set setLicenseUrl(url) {
+    this.licenseUrl = url;
+  }
+
+
+  // login
+  login(params){
+    this.username = params.username
+    this.password = params.password
+
+    console.log("wasabi login");
+    var xhttp = new XMLHttpRequest();
+    xhttp.open("POST", this.rightTvUrl + "/Login?client=json", true);
+    xhttp.withCredentials = true
+    xhttp.setRequestHeader("Content-type", "application/x-www-form-urlencoded");
+    xhttp.send("username=" + this.username +"&password=" + this.password);
+
+    xhttp.onreadystatechange = function() {
+
+        if (this.readyState == 4 && this.status == 200) {
+            console.log("response: " + xhttp.responseText );
+            var status = JSON.parse(xhttp.responseText)["response"]["status"];
+            console.log("response: " + status );
+
+
+            //send ok
+            //sendMessageToSender()
+        }
+    }
+
+  }
+
+
+  get getUsername() {
+    return this.username;
+  }
+
+
+
+}
