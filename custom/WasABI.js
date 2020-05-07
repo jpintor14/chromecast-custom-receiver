0c8aecf9392841acc0bcf691c1df22b51d27ca19
Javier Pintor <javier.pintor@optivamedia.com>
2020-05-07 16:21:11 +0200

    Quit change

--- custom/WasABI.js
+++ custom/WasABI.js
@@ -9,20 +9,16 @@ class WasABI {
  
   constructor() {
     this.chromecastChannel = "urn:x-cast:com.optm.anhplayer";
-    //this.licenseUrl = "https://ios.orangetv.orange.es/mob/api/rtv/v1/drm";
-    this.licenseUrl = "https://orangetv.orange.es/mob/api/rtv/v1/drm";
+    this.licenseUrl = "https://ios.orangetv.orange.es/mob/api/rtv/v1/drm";
 
     //right tv
-    //this.rightTvUrl = "https://ios.orangetv.orange.es/mob/api/rtv/v1";
-    this.rightTvUrl = "https://orangetv.orange.es/mob/api/rtv/v1";
+    this.rightTvUrl = "https://ios.orangetv.orange.es/mob/api/rtv/v1";
     this.loginUri = "Login";
 
     //compass 
-    //this.compassUrl = "https://ios.orangetv.orange.es/mob/api/reco/v1";
-    this.compassUrl = "https://orangetv.orange.es/mob/api/reco/v1";
+    this.compassUrl = "https://ios.orangetv.orange.es/mob/api/reco/v1";
     //session manager
     this.sessionManagerUrl = "https://ios.orangetv.orange.es/mob/api/sm/v1";
-    this.sessionManagerUrl = "https://orangetv.orange.es/mob/api/sm/v1";
     this.openSessionUri = "openSession";
     this.closeSessionUri = "closeSession";
     this.sessionManagerId = "";
